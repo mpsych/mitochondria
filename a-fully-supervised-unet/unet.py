ac4692fbbb14643bbeae48554b0d2c92dbefd010
Daniel Haehn <haehn@mpsych.org>
2022-10-19 13:05:11 -0400

    Retraining the network works with TF 2.2 and the .yml environment.

--- a-fully-supervised-unet/unet.py
+++ a-fully-supervised-unet/unet.py
@@ -57,7 +57,7 @@ def train(model, imgs_train, imgs_mask_train, imgs_test, imgs_mask_test, model_n
     if not os.path.isdir(model_name):
         os.mkdir(model_name)
 
-    sname = model_name + '/weights.{epoch:02d}-{val_loss:.2f}.hdf5'
+    sname = model_name + '/weights.{epoch:02d}-{loss:.2f}.hdf5'
     model_checkpoint = ModelCheckpoint(sname, monitor='val_loss',verbose=1, save_best_only=False, save_freq=5)
     print('saving model checkpoints in', model_name + '/')
     
